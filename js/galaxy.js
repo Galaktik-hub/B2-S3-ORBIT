9886e91ce23556b21f070c660243ab436a2219be
Abdelrmb <richeabdelrahim1@gmail.com>
2024-11-17 14:23:25 +0100

    ajout des planet dans la map

--- js/galaxy.js
+++ js/galaxy.js
@@ -7,14 +7,35 @@ const map = L.map('galaxy-map', {
 
 map.setView([500, 500], -2);
 
+const planetIcons = {
+    Tatooine: L.icon({
+        iconUrl: '../images/tatooine.png',
+        iconSize: [75, 75],
+        iconAnchor: [20, 20],
+        popupAnchor: [0, -20]
+    }),
+    Coruscant: L.icon({
+        iconUrl: '../images/coruscant.png',
+        iconSize: [75, 75],
+        iconAnchor: [20, 20],
+        popupAnchor: [0, -20]
+    }),
+    Alderaan: L.icon({
+        iconUrl: '../images/alderaan.png',
+        iconSize: [75, 75],
+        iconAnchor: [20, 20],
+        popupAnchor: [0, -20]
+    })
+};
+
 const planets = [
-    { name: 'Tatooine', coords: [200, 300] },
-    { name: 'Coruscant', coords: [700, 800] },
-    { name: 'Alderaan', coords: [500, 400] }
+    { name: 'Tatooine', coords: [1200, 600], icon: planetIcons.Tatooine },
+    { name: 'Coruscant', coords: [6710, 7500], icon: planetIcons.Coruscant },
+    { name: 'Alderaan', coords: [-4540, -1500], icon: planetIcons.Alderaan }
 ];
 
 planets.forEach(planet => {
-    L.marker(planet.coords)
+    L.marker(planet.coords, { icon: planet.icon })
         .addTo(map)
         .bindPopup(`<b>${planet.name}</b>`);
 });
