caea21003899fa8e3dc27c8ce9ab42bf994cb7e2
Abdelrmb <richeabdelrahim1@gmail.com>
2024-11-22 01:51:40 +0100

    retrait des infos inutiles

--- page/index.php
+++ page/index.php
@@ -25,41 +25,6 @@ checkLogin();
         <div class="planets"></div>
     </div>
 
-    <header>
-        <div class="container">
-            <h1>Bienvenue sur le Galactic Traveler</h1>
-            <p>Explorez l'univers, connectez-vous avec d'autres voyageurs et découvrez de nouveaux mondes !</p>
-        </div>
-    </header>
-
-    <main>
-        <section class="about" id="about">
-            <h2>À propos de la galaxie</h2>
-            <p>Galactic Traveler est votre passerelle vers un monde d'exploration. Découvrez des planètes, des lunes et des systèmes stellaires en parcourant la galaxie. Rejoignez une communauté d'explorateurs comme vous !</p>
-        </section>
-
-        <section class="features" id="features">
-            <div class="feature-item">
-                <h3>Planètes</h3>
-                <p>Visitez des planètes mystérieuses et explorez leurs environnements uniques.</p>
-            </div>
-            <div class="feature-item">
-                <h3>Communauté</h3>
-                <p>Connectez-vous avec d'autres voyageurs à travers la galaxie.</p>
-            </div>
-            <div class="feature-item">
-                <h3>Systèmes Stellaires</h3>
-                <p>Naviguez à travers différents systèmes stellaires et découvrez des secrets cachés.</p>
-            </div>
-        </section>
-    </main>
-
-    <footer>
-        <div class="container">
-            <p>Développé par l'équipe &copy; 2024</p>
-        </div>
-    </footer>
-
     <script src="../js/starwars.js"></script> 
 </body>
 </html>
