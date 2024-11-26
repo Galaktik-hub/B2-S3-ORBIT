08866655600653070f401b942f3fb17fe2264974
Abdelrmb <richeabdelrahim1@gmail.com>
2024-11-26 11:48:55 +0100

    Added method post verification

--- back/back_login.php
+++ back/back_login.php
@@ -2,33 +2,35 @@
 session_start();
 include 'cnx.php';
 
-$pseudo = $_POST['pseudo'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$sql = "SELECT * FROM users WHERE pseudo = :pseudo AND is_active = 1";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':pseudo', $pseudo);
-$stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
 
-if ($user) {
-    $custom_salt = $user['salt'];
-    $hashed_password_input = $custom_salt . $password;
+    $sql = "SELECT * FROM users WHERE pseudo = :pseudo AND is_active = 1";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':pseudo', $pseudo);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if (password_verify($hashed_password_input, $user['password'])) {
-        $_SESSION['pseudo'] = $user['pseudo'];
-        $_SESSION['id'] = $user['id'];
-        header("Location: ../page/index.php");
-        exit();
+    if ($user) {
+        $custom_salt = $user['salt'];
+        $hashed_password_input = $custom_salt . $password;
+
+        if (password_verify($hashed_password_input, $user['password'])) {
+            $_SESSION['pseudo'] = $user['pseudo'];
+            $_SESSION['id'] = $user['id'];
+            header("Location: ../page/index.php");
+            exit();
+        } else {
+            header("Location: ../index.html?message=Login ou mot de passe incorect&type=error");
+            exit();
+        }
     } else {
         header("Location: ../index.html?message=Login ou mot de passe incorect&type=error");
         exit();
     }
-} else {
-    header("Location: ../index.html?message=Login ou mot de passe incorect&type=error");
-    exit();
-}
 
-$stmt = null;
-$pdo = null;
-?>
+    $stmt = null;
+    $pdo = null;
+}
