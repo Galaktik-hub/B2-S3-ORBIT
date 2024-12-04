87fa1e817b2feca7893af8b5dd5d49fe2f1dc940
JulienS-Code <julien.synaeve@gmail.com>
2024-12-04 10:56:48 +0100

    Push Java project

---
+++ java/src/main/java/fr/starwars/Graphe.java
@@ -0,0 +1,37 @@
+package fr.starwars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graphe {
+    private List<Planete> planetes = new ArrayList<>();
+    private List<Arete> aretes = new ArrayList<>();
+
+    public void ajouterPlanete(Planete planete) {
+        planetes.add(planete);
+    }
+
+    public void ajouterArete(Arete arete) {
+        aretes.add(arete);
+    }
+
+    public List<Planete> getPlanetes() {
+        return planetes;
+    }
+
+    public List<Arete> getAretes() {
+        return aretes;
+    }
+
+    public int getLength() { return aretes.size(); }
+
+    // Trouver une planète par son ID
+    public Planete findPlaneteById(int id) {
+        for (Planete p : this.getPlanetes()) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
